51cc2a56668d073c2203d167e9f8bb316dc51245
meriadec <meriadec.pillet@gmail.com>
2015-03-07 22:30:24 +0100

    style(core): styling home

--- client/directives/nav-bar/nav-bar.directive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('bangular')
-  .directive('navBar', function () {
-    return {
-      restrict: 'EA',
-      templateUrl: 'directives/nav-bar/nav-bar.html'
-    };
-  });
